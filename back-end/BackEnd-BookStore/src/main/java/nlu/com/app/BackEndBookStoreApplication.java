800b988fe2e0fe1118d89e1654eedd66d80036db
tuan0919 <21130601@st.hcmuaf.edu.vn>
2025-03-23 18:32:22 +0700

    Initial project structure for Back-End

---
+++ back-end/BackEnd-BookStore/src/main/java/nlu/com/app/BackEndBookStoreApplication.java
@@ -0,0 +1,13 @@
+package nlu.com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndBookStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndBookStoreApplication.class, args);
+	}
+
+}
