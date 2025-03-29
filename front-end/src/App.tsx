83c005d4c3334769d5874c8fbd6aa84eccc93545
tuan0919 <21130601@st.hcmuaf.edu.vn>
2025-03-29 09:52:58 +0700

    Add Browser React Router to front-end project

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,9 +1,17 @@
+import { Navigate, Route, Routes } from "react-router-dom"
 import { Home } from "./pages/Home"
+import { Box, Typography } from "@mui/material"
 
 function App() {
   return (
     <>
-      <Home/>
+      <Routes>
+        <Route path="/" element={<Navigate to="/home" />} />
+        <Route path="/category" element={undefined} />
+        <Route path="/details" element={undefined} />
+        <Route path="/home" Component={Home} />
+        <Route path="*" element={<Box><Typography>Không tồn tại</Typography></Box>} />
+      </Routes>
     </>
   )
 }
