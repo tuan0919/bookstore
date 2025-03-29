1917f75e0f1789be60f5acf1770856ebdc2bfd5b
tuan0919 <21130601@st.hcmuaf.edu.vn>
2025-03-30 03:40:27 +0700

    Update BookDetails

--- front-end/src/pages/BookDetails/index.tsx
+++ front-end/src/pages/BookDetails/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Breadcrumbs, Button, Container, Link, Typography } from "@mui/material";
+import { Box, Breadcrumbs, Button, Container, Link, Stack, styled, Typography } from "@mui/material";
 import 'lightgallery/css/lightgallery.css';
 import 'lightgallery/css/lg-zoom.css';
 import 'lightgallery/css/lg-thumbnail.css';
@@ -6,6 +6,18 @@ import { BookGallery } from "./BookGallery";
 import { BookGalleryMocks } from "~/mocks/BookGalleryMocks";
 import { red } from "@mui/material/colors";
 import AddShoppingCartRoundedIcon from '@mui/icons-material/AddShoppingCartRounded';
+import { BookOverview } from "./BookOverview";
+
+const CustomizeBox = styled(Box)(({ theme }) => ({
+    display: 'flex',
+    flexDirection: 'column',
+    backgroundColor: 'white',
+    borderRadius: 8,
+    padding: theme.spacing(2),
+    alignItems: 'center',
+    gap: theme.spacing(2),
+}));
+
 export function BookDetails() {
     const breadcrumbs = [
         <Link underline="hover" key="1" color="inherit" href="/">
@@ -28,36 +40,33 @@ export function BookDetails() {
             <Breadcrumbs sx={{ marginY: 3 }} separator="›" aria-label="breadcrumb">
                 {breadcrumbs}
             </Breadcrumbs>
-            <Box sx={{
-                display: 'inline-flex',
-                flexDirection: 'column',
-                backgroundColor: 'white',
-                borderRadius: 2,
-                padding: 2,
-                alignItems: 'center',
-                gap: 2,
-            }}>
-                <BookGallery gallery={BookGalleryMocks} />
-                <Box display={'flex'} width={'100%'} gap={2}>
-                    <Button variant="outlined"
-                        disableTouchRipple
-                        color="error"
-                        startIcon={<AddShoppingCartRoundedIcon />}
-                        sx={{ flex: 1, borderWidth: 2, borderColor: red['A700'], textTransform: 'none' }}>
-                        <Typography fontWeight={'bold'}>
-                            Thêm vào giỏ hàng
-                        </Typography>
-                    </Button>
-                    <Button variant="contained"
-                        disableTouchRipple
-                        color="error"
-                        sx={{ flex: 1, textTransform: 'none' }}>
-                        <Typography fontWeight={'bold'}>
-                            Mua ngay
-                        </Typography>
-                    </Button>
-                </Box>
-            </Box>
+            <Stack direction={'row'} spacing={2}>
+                <CustomizeBox>
+                    <BookGallery gallery={BookGalleryMocks} />
+                    <Box display={'flex'} width={'100%'} gap={2}>
+                        <Button variant="outlined"
+                            disableTouchRipple
+                            color="error"
+                            startIcon={<AddShoppingCartRoundedIcon />}
+                            sx={{ flex: 1, borderWidth: 2, borderColor: red['A700'], textTransform: 'none' }}>
+                            <Typography fontWeight={'bold'}>
+                                Thêm vào giỏ hàng
+                            </Typography>
+                        </Button>
+                        <Button variant="contained"
+                            disableTouchRipple
+                            color="error"
+                            sx={{ flex: 1, textTransform: 'none' }}>
+                            <Typography fontWeight={'bold'}>
+                                Mua ngay
+                            </Typography>
+                        </Button>
+                    </Box>
+                </CustomizeBox>
+                <CustomizeBox sx={{ flex: 1, height: 'fit-content' }}>
+                    <BookOverview sx={{ width: '100%' }} />
+                </CustomizeBox>
+            </Stack>
         </Container>
     )
 }
