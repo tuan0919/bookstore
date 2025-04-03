e3a5adaee50a77ebc3dbdd595e9f2428d9e1e263
VuHoangMinhTu <21130598@st.hcmuaf.edu.vn>
2025-04-03 07:52:37 +0700

    Lam trang Category

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,8 +1,11 @@
-import { Navigate, Route, Routes } from "react-router-dom"
-import { Home } from "./pages/Home"
-import { Box, Typography } from "@mui/material"
-import { BookDetails } from "./pages/BookDetails"
-import { MainLayout } from "./components/MainLayout"
+import { Navigate, Route, Routes } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { Box, Typography } from "@mui/material";
+
+import { BookDetails } from "./pages/BookDetails";
+import { MainLayout } from "./components/MainLayout";
+
+import CategoryPage from "./pages/Category";
 
 function App() {
   return (
@@ -13,11 +16,19 @@ function App() {
           <Route path="/category" element={undefined} />
           <Route path="/details" Component={BookDetails} />
           <Route path="/home" Component={Home} />
-          <Route path="*" element={<Box><Typography>Không tồn tại</Typography></Box>} />
+          <Route
+            path="*"
+            element={
+              <Box>
+                <Typography>Không tồn tại</Typography>
+              </Box>
+            }
+          />
+          <Route path="/category/*" element={<CategoryPage />} />
         </Route>
       </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
