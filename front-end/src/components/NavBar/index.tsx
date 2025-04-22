ec675da6b0a18f01038b2f1288cbb813b2ec20d7
tuan0919 <21130601@st.hcmuaf.edu.vn>
2025-04-22 20:13:37 +0700

    Change Logo

--- front-end/src/components/NavBar/index.tsx
+++ front-end/src/components/NavBar/index.tsx
@@ -1,62 +1,77 @@
-import { Box, Container, Grid2, Stack } from '@mui/material';
-import { red } from '@mui/material/colors';
-import topBanner from '~/assets/top-banner.png';
-import logo from '~/assets/logo.png';
-import { NotificationButton } from './NotificationButton';
-import { ProfileButton } from './ProfileButton';
-import { LanguageButton } from './LanguageButton';
-import { SearchField } from './SearchField';
-import { CategoryButton } from './CategoryButton';
-import { CartButton } from './CartButton';
+import { Box, Container, Grid2, Stack } from "@mui/material";
+import { red } from "@mui/material/colors";
+import topBanner from "~/assets/top-banner.png";
+import logo from "~/assets/logo2.png";
+import { NotificationButton } from "./NotificationButton";
+import { ProfileButton } from "./ProfileButton";
+import { LanguageButton } from "./LanguageButton";
+import { SearchField } from "./SearchField";
+import { CategoryButton } from "./CategoryButton";
+import { CartButton } from "./CartButton";
 
 export function NavBar() {
-  console.log('NavBar re-render')
+  console.log("NavBar re-render");
   return (
-    <Stack direction='column' sx={{ backgroundColor: 'white' }}>
-      <Box sx={{ backgroundColor: '#C92127' }}>
+    <Stack direction="column" sx={{ backgroundColor: "white" }}>
+      <Box sx={{ backgroundColor: "#C92127" }}>
         <Container>
-          <Box component='div' sx={{
-            position: "relative",
-            width: '100%',
-            display: { xs: 'none', sm: 'block' }
-          }}>
-            <img src={topBanner} width={'100%'} alt={''} />
+          <Box
+            component="div"
+            sx={{
+              position: "relative",
+              width: "100%",
+              display: { xs: "none", sm: "block" },
+            }}
+          >
+            <img src={topBanner} width={"100%"} alt={""} />
           </Box>
         </Container>
       </Box>
-      <Container sx={{
-        backgroundColor: { md: 'transparent', xs: red['600'] }
-      }}>
-        <Grid2 container
-          paddingY={{ xs: 1, md: 2 }} spacing={1} sx={{
-            height: { md: '80px' },
-            justifyContent: 'space-between',
-            alignItems: 'center',
-          }}>
+      <Container
+        sx={{
+          backgroundColor: { md: "transparent", xs: red["600"] },
+        }}
+      >
+        <Grid2
+          container
+          paddingY={{ xs: 1, md: 2 }}
+          spacing={1}
+          sx={{
+            height: { md: "auto" },
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
           <Grid2
-            sx={{ justifyContent: 'center', display: 'flex' }}
-            size={{ xs: 12, md: 3 }}>
+            sx={{ justifyContent: "center", display: "flex" }}
+            size={{ xs: 12, md: 3 }}
+          >
             <Box
               sx={{
                 position: "relative",
-                display: 'flex',
-                alignItems: 'center',
-                justifyContent: 'center',
-                height: { xs: '100%', md: '100%' },
-                width: { xs: '50%', md: '80%' }
-              }}>
-              <img src={logo} alt="Logo" width={'100%'} style={{ objectFit: "contain" }}
-              />
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                height: { xs: "5rem", md: "4rem" },
+                width: { xs: "5rem", md: "12rem" },
+              }}
+            >
+              <img src={logo} alt="Logo" width={"100%"} />
             </Box>
           </Grid2>
           <Grid2 size={{ xs: 7, md: 3 }}>
-            <Stack direction='row' sx={{ gap: 1, justifyContent: 'center' }}>
+            <Stack direction="row" sx={{ gap: 1, justifyContent: "center" }}>
               <CategoryButton />
               <SearchField />
             </Stack>
           </Grid2>
           <Grid2 size={{ xs: 5, md: 3 }}>
-            <Stack alignItems={'center'} justifyContent={'flex-end'} direction={'row'} spacing={2}>
+            <Stack
+              alignItems={"center"}
+              justifyContent={"flex-end"}
+              direction={"row"}
+              spacing={2}
+            >
               <NotificationButton />
               <CartButton />
               <ProfileButton />
@@ -66,5 +81,5 @@ export function NavBar() {
         </Grid2>
       </Container>
     </Stack>
-  )
+  );
 }
--- front-end/src/pages/Home/Carousel/index.tsx
+++ front-end/src/pages/Home/Carousel/index.tsx
@@ -1,30 +1,41 @@
-import 'swiper/swiper-bundle.css'
-import { Swiper, SwiperSlide } from 'swiper/react';
+import "swiper/swiper-bundle.css";
+import { Swiper, SwiperSlide } from "swiper/react";
 import { Pagination } from "swiper/modules";
-import banner_1 from '~/assets/banner/banner_1.png';
-import banner_2 from '~/assets/banner/banner_2.png';
-import banner_3 from '~/assets/banner/banner_3.png';
-import { Box } from '@mui/material';
+import banner_1 from "~/assets/banner/banner_1.png";
+import banner_2 from "~/assets/banner/banner_2.png";
+import banner_3 from "~/assets/banner/banner_3.png";
+import banner_manga from "~/assets/banner/image.png";
+import { Box } from "@mui/material";
 
 export function Carousel() {
-    return (
-        <Box sx={{
-            borderRadius: 3,
-            overflow: 'hidden',
-            height: 438,
-            cursor: 'pointer',
-        }}>
-            <Swiper loop={true} style={{height: '100%'}} pagination={{ clickable: true }} modules={[Pagination]}>
-                <SwiperSlide>
-                    <img src={banner_1} width={'100%'}/>   
-                </SwiperSlide>
-                <SwiperSlide>
-                    <img src={banner_2} width={'100%'}/>
-                </SwiperSlide>
-                <SwiperSlide>
-                    <img src={banner_3} width={'100%'}/>
-                </SwiperSlide>
-            </Swiper>
-        </Box>
-    )
+  return (
+    <Box
+      sx={{
+        borderRadius: 3,
+        overflow: "hidden",
+        height: 438,
+        cursor: "pointer",
+      }}
+    >
+      <Swiper
+        loop={true}
+        style={{ height: "100%" }}
+        pagination={{ clickable: true }}
+        modules={[Pagination]}
+      >
+        <SwiperSlide>
+          <img src={banner_manga} width={"100%"} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={banner_1} width={"100%"} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={banner_2} width={"100%"} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={banner_3} width={"100%"} />
+        </SwiperSlide>
+      </Swiper>
+    </Box>
+  );
 }
