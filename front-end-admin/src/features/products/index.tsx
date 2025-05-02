7cb3a25e83e0d4acfd1239a94220c0b4ebf21d53
tuan0919 <21130601@st.hcmuaf.edu.vn>
2025-05-02 23:36:05 +0700

    remake product's overview UI

--- front-end-admin/src/features/products/index.tsx
+++ front-end-admin/src/features/products/index.tsx
@@ -1,15 +1,6 @@
-import { useState } from 'react'
 import { z } from 'zod'
 import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
-import { Link } from '@tanstack/react-router'
-import {
-  IconPencil,
-  IconSearch,
-  IconTrash,
-  IconGift,
-  IconPlus,
-} from '@tabler/icons-react'
 import { Button } from '@/components/ui/button'
 import {
   Card,
@@ -26,35 +17,14 @@ import {
   FormItem,
   FormLabel,
 } from '@/components/ui/form'
-import { Input } from '@/components/ui/input'
-import {
-  Pagination,
-  PaginationContent,
-  PaginationEllipsis,
-  PaginationItem,
-  PaginationLink,
-  PaginationNext,
-  PaginationPrevious,
-} from '@/components/ui/pagination'
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from '@/components/ui/select'
 import { Slider } from '@/components/ui/slider'
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipTrigger,
-} from '@/components/ui/tooltip'
 import { Header } from '@/components/layout/header'
 import { Main } from '@/components/layout/main'
 import { TopNav } from '@/components/layout/top-nav'
 import { ProfileDropdown } from '@/components/profile-dropdown'
 import { Search } from '@/components/search'
 import { ThemeSwitch } from '@/components/theme-switch'
+import { DataTableDemo } from './components/product-data-datable'
 
 const formSchema = z.object({
   languages: z.string().array(),
@@ -74,8 +44,7 @@ export default function Product() {
   })
 
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof formSchema>) {}
-  const [limit, setLimit] = useState('10')
+  function onSubmit(_: z.infer<typeof formSchema>) {}
   return (
     <>
       {/* ===== Top Heading ===== */}
@@ -113,7 +82,7 @@ export default function Product() {
                   <FormField
                     control={form.control}
                     name='languages'
-                    render={({ field }) => (
+                    render={() => (
                       <>
                         <FormItem className='flex flex-row items-center space-y-0 space-x-3'>
                           <FormControl>
@@ -134,7 +103,7 @@ export default function Product() {
                   <FormField
                     control={form.control}
                     name='genres'
-                    render={({ field }) => (
+                    render={() => (
                       <>
                         <FormItem className='flex flex-row items-center space-y-0 space-x-3'>
                           <FormControl>
@@ -173,7 +142,7 @@ export default function Product() {
                   <FormField
                     control={form.control}
                     name='priceRange'
-                    render={({ field }) => (
+                    render={() => (
                       <Slider defaultValue={[50]} max={100} step={1} />
                     )}
                   />
@@ -183,134 +152,7 @@ export default function Product() {
             </CardContent>
           </Card>
           <div className='col-span-3 flex-col'>
-            <Card className='w-full'>
-              <CardHeader>
-                <CardTitle className='mb-2'>Tìm kiếm theo tên</CardTitle>
-                <div className='flex items-center justify-between gap-4'>
-                  {/* Search Input */}
-                  <div className='relative w-full max-w-sm'>
-                    <IconSearch
-                      size={20}
-                      className='text-muted-foreground absolute top-1/2 left-3 -translate-y-1/2'
-                    />
-                    <Input
-                      type='text'
-                      placeholder='Tìm kiếm...'
-                      className='pl-10'
-                    />
-                  </div>
-
-                  <div className='flex gap-2'>
-                    <Select value={limit} onValueChange={setLimit}>
-                      <SelectTrigger>
-                        <SelectValue placeholder='Số lượng' />
-                      </SelectTrigger>
-                      <SelectContent>
-                        <SelectItem value='10'>10 sản phẩm</SelectItem>
-                        <SelectItem value='20'>20 sản phẩm</SelectItem>
-                        <SelectItem value='40'>40 sản phẩm</SelectItem>
-                      </SelectContent>
-                    </Select>
-                    <Link to='/products/new'>
-                      <Button variant='outline' className='gap-1'>
-                        <IconPlus size={18} />
-                        Thêm sản phẩm
-                      </Button>
-                    </Link>
-                  </div>
-                </div>
-              </CardHeader>
-            </Card>
-            <div className='grid w-full grid-cols-4 gap-3 py-3'>
-              {Array.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10).map((value, index) => (
-                <Card
-                  key={index}
-                  className='cursor-pointer gap-1 px-2 py-1 transition duration-200 ease-in-out hover:shadow-lg'
-                >
-                  <CardHeader className='group relative overflow-hidden rounded-sm'>
-                    <div className='absolute top-0 left-0 rounded-br-md bg-red-500 px-2 py-1 text-xs font-bold text-white'>
-                      -10%
-                    </div>
-                    <div className='absolute inset-0 z-10 bg-black/40 opacity-0 transition duration-500 group-hover:opacity-100' />
-                    <div className='absolute right-2 bottom-2 z-20 flex gap-2 opacity-0 transition duration-500 group-hover:opacity-100'>
-                      <Tooltip>
-                        <TooltipTrigger asChild>
-                          <button className='flex h-8 w-8 items-center justify-center rounded-full bg-yellow-400 text-white hover:brightness-110'>
-                            <IconPencil size={16} />
-                          </button>
-                        </TooltipTrigger>
-                        <TooltipContent>
-                          <p>Chỉnh sửa</p>
-                        </TooltipContent>
-                      </Tooltip>
-
-                      <Tooltip>
-                        <TooltipTrigger asChild>
-                          <button className='flex h-8 w-8 items-center justify-center rounded-full bg-green-500 text-white hover:brightness-110'>
-                            <IconGift size={16} />
-                          </button>
-                        </TooltipTrigger>
-                        <TooltipContent>
-                          <p>Khuyến mãi</p>
-                        </TooltipContent>
-                      </Tooltip>
-
-                      <Tooltip>
-                        <TooltipTrigger asChild>
-                          <button className='flex h-8 w-8 items-center justify-center rounded-full bg-red-500 text-white hover:brightness-110'>
-                            <IconTrash size={16} />
-                          </button>
-                        </TooltipTrigger>
-                        <TooltipContent>
-                          <p>Xóa sản phẩm</p>
-                        </TooltipContent>
-                      </Tooltip>
-                    </div>
-                    <img src='https://nhasachphuongnam.com/images/detailed/277/manga-duoc-su-tu-su-tap-13.jpg' />
-                  </CardHeader>
-                  <CardContent className='flex flex-col gap-2 px-2'>
-                    <h2
-                      className='text-sm'
-                      style={{
-                        fontFamily: 'Segoe UI',
-                        fontSize: 15,
-                        fontWeight: 600,
-                      }}
-                    >
-                      Dược sư tự sự (Manga) - Tập 3
-                    </h2>
-                    <div className='flex w-full justify-between'>
-                      <p className='text-sm font-semibold'>39,950₫</p>
-                      <p className='text-sm line-through'>47,000₫</p>
-                    </div>
-                  </CardContent>
-                </Card>
-              ))}
-            </div>
-            <Pagination className='mt-4'>
-              <PaginationContent>
-                <PaginationItem>
-                  <PaginationPrevious href='#' />
-                </PaginationItem>
-                <PaginationItem>
-                  <PaginationLink href='#'>1</PaginationLink>
-                </PaginationItem>
-                <PaginationItem>
-                  <PaginationLink href='#' isActive>
-                    2
-                  </PaginationLink>
-                </PaginationItem>
-                <PaginationItem>
-                  <PaginationLink href='#'>3</PaginationLink>
-                </PaginationItem>
-                <PaginationItem>
-                  <PaginationEllipsis />
-                </PaginationItem>
-                <PaginationItem>
-                  <PaginationNext href='#' />
-                </PaginationItem>
-              </PaginationContent>
-            </Pagination>
+            <DataTableDemo />
           </div>
         </div>
       </Main>
