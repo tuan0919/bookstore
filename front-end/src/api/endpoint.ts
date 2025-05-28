38e058a19bd4a2d70a0f923f7fc9a9201b1eda74
tuan0919 <21130601@st.hcmuaf.edu.vn>
2025-05-28 22:50:40 +0700

    apply auth context for entire application

--- front-end/src/api/endpoint.ts
+++ front-end/src/api/endpoint.ts
@@ -1,6 +1,7 @@
-// src/constants/endpoint.ts
-
 const API_ENDPOINTS = {
+  AUTH: {
+    LOGIN: "/api/v1/auth/login",
+  },
   BOOK: {
     SEARCH: "/api/book/category",
     TOP_WEEKLY: "/api/book/top-weekly",
@@ -8,7 +9,7 @@ const API_ENDPOINTS = {
   },
   CATEGORY: {
     CHAIN_FOR_BOOK: `/api/category/chain`,
-    CATEGORY : "api/book"
+    CATEGORY: "api/book",
   },
 };
 
--- front-end/src/components/NavBar/ProfileButton/index.tsx
+++ front-end/src/components/NavBar/ProfileButton/index.tsx
@@ -1,17 +1,23 @@
 import { Box, Button, Paper, Stack, Typography } from "@mui/material";
-import { grey } from "@mui/material/colors";
+import { grey, red } from "@mui/material/colors";
 import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
 import LoginPopup from "~/components/Popup/login";
 import RegisterPopup from "~/components/Popup/register";
 import VerifyPopup from "~/components/Popup/verifyOTP"; // Import popup xác minh tài khoản
 import { useState } from "react";
+import { useAuthContext } from "~/context/AuthContext";
+import ReceiptOutlinedIcon from "@mui/icons-material/ReceiptOutlined";
+import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
+import LogoutOutlinedIcon from "@mui/icons-material/LogoutOutlined";
+import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
 
 export function ProfileButton() {
   const [isPopupLoginOpen, setPopupLoginOpen] = useState(false);
   const [isPopupRegisterOpen, setPopupRegisterOpen] = useState(false);
   const [isPopupVerifyOpen, setPopupVerifyOpen] = useState(false); // Thêm state cho popup xác minh
-  const [email, setEmail] = useState(""); // 
-  const [password, setPassword] = useState(""); 
+  const [email, setEmail] = useState(""); //
+  const [password, setPassword] = useState("");
+  const { jwtToken } = useAuthContext();
   // Mở popup login
   const handleOpenPopup = () => {
     setPopupLoginOpen(true);
@@ -33,7 +39,6 @@ export function ProfileButton() {
   };
 
   // Mở popup xác minh tài khoản
- 
 
   // Đóng popup xác minh tài khoản
   const handleClosePopupVerify = () => {
@@ -65,7 +70,9 @@ export function ProfileButton() {
           },
         }}
       >
-        <PersonOutlineOutlinedIcon sx={{ fontSize: 30, color: { xs: grey[200], md: grey[600] } }} />
+        <PersonOutlineOutlinedIcon
+          sx={{ fontSize: 30, color: { xs: grey[200], md: grey[600] } }}
+        />
         <Typography
           sx={{
             color: grey[600],
@@ -84,7 +91,6 @@ export function ProfileButton() {
           position: "absolute",
           top: 50,
           right: 0,
-          p: 1,
           width: 200,
           bgcolor: "white",
           borderRadius: 1,
@@ -101,18 +107,113 @@ export function ProfileButton() {
           },
         }}
       >
-        <Button variant="contained" color="error" onClick={handleOpenPopup}>
-          Đăng nhập
-        </Button>
-        <LoginPopup open={isPopupLoginOpen} onClose={handleClosePopup} />
+        <>
+          {!jwtToken ? (
+            <Box sx={{ p: 1, display: "grid", gap: 1 }}>
+              <Button
+                variant="contained"
+                color="error"
+                onClick={handleOpenPopup}
+              >
+                Đăng nhập
+              </Button>
+              <Button
+                variant="outlined"
+                color="error"
+                onClick={handleOpenPopupRegister}
+              >
+                Đăng ký
+              </Button>
+            </Box>
+          ) : (
+            <>
+              <Box
+                sx={{
+                  display: "flex",
+                  alignItems: "center",
+                  gap: 1,
+                  justifyContent: "center",
+                  p: 1,
+                  mt: 1,
+                }}
+              >
+                <Button
+                  variant="contained"
+                  color="error"
+                  size="small"
+                  startIcon={<AccountCircleOutlinedIcon />}
+                >
+                  Xem trang cá nhân
+                </Button>
+              </Box>
 
-        <Button variant="outlined" color="error" onClick={handleOpenPopupRegister}>
-          Đăng ký
-        </Button>
-        <RegisterPopup open={isPopupRegisterOpen} onClose={handleClosePopupRegister} onRegisterSuccess={handleRegisterSuccess} />
-
-        <VerifyPopup open={isPopupVerifyOpen} onClose={handleClosePopupVerify} onBackToRegister={handleOpenPopupRegister} email={email} password={password} />
+              <Box
+                sx={{
+                  display: "flex",
+                  alignItems: "center",
+                  gap: 1,
+                  cursor: "pointer",
+                  p: 1,
+                  ":hover": {
+                    backgroundColor: grey["100"],
+                  },
+                }}
+              >
+                <ReceiptOutlinedIcon fontSize="small" />
+                <Typography fontWeight={"light"}>Đơn hàng của tôi</Typography>
+              </Box>
+              <Box
+                sx={{
+                  display: "flex",
+                  alignItems: "center",
+                  gap: 1,
+                  cursor: "pointer",
+                  p: 1,
+                  ":hover": {
+                    backgroundColor: grey["100"],
+                  },
+                }}
+              >
+                <FavoriteBorderOutlinedIcon fontSize="small" />
+                <Typography fontWeight={"light"}>Danh sách ưa thích</Typography>
+              </Box>
+              <Box
+                sx={{
+                  display: "flex",
+                  alignItems: "center",
+                  gap: 1,
+                  cursor: "pointer",
+                  p: 1,
+                  ":hover": {
+                    backgroundColor: grey["100"],
+                  },
+                }}
+              >
+                <LogoutOutlinedIcon
+                  fontSize="small"
+                  sx={{ color: red["400"] }}
+                />
+                <Typography fontWeight={"light"} sx={{ color: red["400"] }}>
+                  Đăng xuất
+                </Typography>
+              </Box>
+            </>
+          )}
+        </>
       </Paper>
+      <LoginPopup open={isPopupLoginOpen} onClose={handleClosePopup} />
+      <RegisterPopup
+        open={isPopupRegisterOpen}
+        onClose={handleClosePopupRegister}
+        onRegisterSuccess={handleRegisterSuccess}
+      />
+      <VerifyPopup
+        open={isPopupVerifyOpen}
+        onClose={handleClosePopupVerify}
+        onBackToRegister={handleOpenPopupRegister}
+        email={email}
+        password={password}
+      />
     </Box>
   );
 }
