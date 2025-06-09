cfac6114051613f1afaf22b0447759958a6d52b9
luunguyenthanhvu <21130612@st.hcmuaf.edu.vn>
2025-06-09 20:18:56 +0700

    FEAT: address updated

--- back-end/src/main/java/nlu/com/app/mapper/UserAddressMapper.java
+++ back-end/src/main/java/nlu/com/app/mapper/UserAddressMapper.java
@@ -3,6 +3,7 @@ package nlu.com.app.mapper;
 import nlu.com.app.dto.request.UserAddressDto;
 import nlu.com.app.entity.UserAddress;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 /**
  * @author VuLuu
@@ -10,6 +11,8 @@ import org.mapstruct.Mapper;
 @Mapper(componentModel = "spring", uses = AddressMapper.class)
 public interface UserAddressMapper {
 
+
+  @Mapping(source = "userAddressId", target = "id")
   UserAddressDto toDto(UserAddress entity);
 }
 
