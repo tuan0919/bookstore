2aac7e98c56a307b25a74771379629ce6903644b
Tuan <21130601@st.hcmuaf.edu.vn>
2025-06-18 16:05:42 +0700

    add some new endpoints for promotions

--- back-end/src/main/java/nlu/com/app/service/IPromotionService.java
+++ back-end/src/main/java/nlu/com/app/service/IPromotionService.java
@@ -5,10 +5,13 @@ import nlu.com.app.dto.response.PromotionResponseDTO;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import java.util.List;
+
 /**
  * @author Nguyen Tuan
  */
 public interface IPromotionService {
     PromotionResponseDTO create(CreatePromotionRequest requestDTO);
     Page<PromotionResponseDTO> findAllByPage(Pageable pageable);
+    List<PromotionResponseDTO> getPromotionsAppliedForCategory(Long categoryId);
 }
